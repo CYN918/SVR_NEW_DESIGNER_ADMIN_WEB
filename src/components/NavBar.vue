35005486ed1fe219c0d386aa543d5f56fd59af1f
Chenyinan <18322913588@163.com>
2020-02-17 17:32:02 +0800

    debug

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,27 +6,27 @@
 			</el-breadcrumb>
 		</div>
 		<div class="fright hnav marginright60" style="position: relative;">
-			<router-link to="/review/publishWork" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) || this.auditTitle == '作品发布'">
+			<router-link to="/review/publishWork" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) && this.auditTitle == '作品发布'">
 				<span class="dp fontsize18">审核台</span>
 				<span class="dp sel-badge" v-html="reviewnum">99+</span>
 			</router-link>
-			<router-link to="/review/finalistsWork" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) || this.auditTitle == '作品入围'">
+			<router-link to="/review/finalistsWork" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) && this.auditTitle == '作品入围'">
 				<span class="dp fontsize18">审核台</span>
 				<span class="dp sel-badge" v-html="reviewnum">99+</span>
 			</router-link>
-			<router-link to="/review/employWork" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) || this.auditTitle == '作品录用'">
+			<router-link to="/review/employWork" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) && this.auditTitle == '作品录用'">
 				<span class="dp fontsize18">审核台</span>
 				<span class="dp sel-badge" v-html="reviewnum">99+</span>
 			</router-link>
-			<router-link to="/review/projectreview/projectrepending" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) || this.auditTitle == '项目验收'">
+			<router-link to="/review/projectreview/projectrepending" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) && this.auditTitle == '项目验收'">
 				<span class="dp fontsize18">审核台</span>
 				<span class="dp sel-badge" v-html="reviewnum">99+</span>
 			</router-link>
-			<router-link to="/review/projectreview/projectreallrecords" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) || (adminuseraccess.indexOf('52') > -1) && this.auditTitle == '项目验收'">
+			<router-link to="/review/projectreview/projectreallrecords" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) && (adminuseraccess.indexOf('52') > -1) && this.auditTitle == '项目验收'">
 				<span class="dp fontsize18">审核台</span>
 				<span class="dp sel-badge" v-html="reviewnum">99+</span>
 			</router-link>
-			<router-link to="/review/applyPerson" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) || this.auditTitle == '供稿人申请'">
+			<router-link to="/review/applyPerson" tag="div" class="fleft pointer" v-if="(adminuseraccess.indexOf('11') > -1) && this.auditTitle == '供稿人申请'">
 				<span class="dp fontsize18">审核台</span>
 				<span class="dp sel-badge" v-html="reviewnum">99+</span>
 			</router-link>
