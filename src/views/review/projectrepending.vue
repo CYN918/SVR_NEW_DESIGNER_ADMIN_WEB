1a1b0049f6814a96550fa52ddcef09c3609c8499
陈怿男 <18322913588@163.com>
2020-03-03 11:55:38 +0800

    debug

--- src/views/review/projectrepending.vue
+++ src/views/review/projectrepending.vue
@@ -186,7 +186,7 @@
 					limit: pg.pageSize,
 					type:5,
 					check_steps: this.check_steps_status,
-					per_check_name: this.$route.query.user,
+					// per_check_name: this.$route.query.user,
 					check_status:0,
 					business_type: this.business_id.substring(0,this.business_id.lastIndexOf(',')),
 				}
