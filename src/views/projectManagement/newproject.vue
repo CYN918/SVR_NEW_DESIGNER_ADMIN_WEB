96f3a091dd8bca8ad784376dfb89ef7f4b6bd015
陈怿男 <18322913588@163.com>
2020-03-14 19:59:29 +0800

    debug

--- src/views/projectManagement/newproject.vue
+++ src/views/projectManagement/newproject.vue
@@ -89,14 +89,14 @@
 							</ul>
 						</div>
 					</li>
-					<li class="margint23 ofh">
+					<!-- <li class="margint23 ofh">
 						<span class="fleft detailKey" style="line-height: 40px;">结算方式</span>
 						<el-radio-group v-model="form['want_deal_type']">
 							<el-radio-button label="0">用户选择</el-radio-button>
 							<el-radio-button label="1">买断</el-radio-button>
 							<el-radio-button label="2">分成</el-radio-button>
 						</el-radio-group>
-					</li>
+					</li> -->
 				</ul>
 				<ul style="border-top: 1px solid #E6E6E6;padding-top: 40px;margin-top: 40px;">
 					<li class="margint23 ofh">
@@ -148,10 +148,10 @@
 						<span class="fleft detailKey" style="line-height: 40px;">预计收益</span>
 						<el-input placeholder="给创作者的收益预期,如: 20.00~30.00/张" v-model="form['expected_profit']" style="width:357px;height:40px;" clearable></el-input>
 					</li>
-					<!-- <li class="margint23 ofh">
+					<li class="margint23 ofh">
 						<span class="fleft detailKey" style="line-height: 40px;">额外赏金</span>
 						<el-input placeholder="单位, 元; 额外赏金可吸引更多创作者报名" v-model="form['extra_reward']" style="width:357px;height:40px;" clearable></el-input>
-					</li> -->
+					</li>
 					<li class="margint23 ofh">
 						<span class="fleft detailKey" style="line-height: 40px;">项目顾问QQ</span>
 						<el-input placeholder="请填写QQ号, 项目顾问将负责解决创作者的疑问" v-model="form['qq']" style="width:357px;height:40px;" clearable></el-input>
