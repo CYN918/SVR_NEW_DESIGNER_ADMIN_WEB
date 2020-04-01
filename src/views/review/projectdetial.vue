10760b98a8e5249f54b5b5f449a8724624fbd2bd
陈怿男 <18322913588@163.com>
2020-04-01 15:29:17 +0800

    debug

--- src/views/review/projectdetial.vue
+++ src/views/review/projectdetial.vue
@@ -213,7 +213,7 @@
 							</el-select>
 						</div>
 					</li> -->
-					<li class="w ofh" v-if="material_info.type == '1' && is_ruku == '2'">
+					<li class="w ofh" v-if="material_info.type == '1' && is_ruku == '0'">
 						<div class="textcenter employipt">
 							<span class="fleft Dialogkey" style="width: 84px;text-align: right;">入库素材数量</span>
 							<el-input v-model="storage_number" placeholder="请输入内容" style="width: 300px;float: left;"></el-input>
@@ -700,7 +700,7 @@
 						})
 						return;
 					}
-					if(this.is_ruku == '2'){
+					if(this.is_ruku == '0'){
 						if(!this.storage_number){
 							this.$message({
 								message: "请填写入库素材数量!",
