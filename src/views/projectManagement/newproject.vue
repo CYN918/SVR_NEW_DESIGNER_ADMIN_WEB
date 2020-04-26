c9609a0c8010490ce57f2adf61222be9df5c2b75
陈怿男 <18322913588@163.com>
2020-04-26 19:25:20 +0800

    debug

--- src/views/projectManagement/newproject.vue
+++ src/views/projectManagement/newproject.vue
@@ -110,6 +110,14 @@
 						<img v-if="form['banner']" :src="form['banner']" alt="" width="340px" height="110px" style="margin-left: 156px;">
 					</li>
 					<li class="margint23 ofh">
+							<span class="fleft detailKey" style="line-height: 40px;">详情banner</span>
+							<el-upload class="upload" action="454535" :http-request="httprequestbanner" :show-file-list="false">
+								<button class="defaultbtn" style="margin-left: 0;">{{ form['detail_banner'] ? "重新上传" : "上传图片" }}</button>
+								<div class="fontcolorg">1920px*480px，格式jpg，jpeg，png，大小不超过10M</div>
+							</el-upload>
+							<img v-if="form['detail_banner']" :src="form['detail_banner']" alt="" width="340px" height="110px" style="margin-left: 156px;">
+						</li>
+					<li class="margint23 ofh">
 						<span class="fleft detailKey" style="line-height: 40px;">领域范围</span>
 						<div>
 							<div class="ofh">
@@ -371,6 +379,7 @@
 					name:'',
 					rule_type : 1,
 					banner:'',
+					detail_banner:'',
 					access_token: localStorage.getItem("access_token"),
 					business_type:"",
 					classify_id:"",
@@ -619,6 +628,7 @@
 						{prop:'template_name',lable:'模板名称'},
 						{prop:'classify_name',lable:'所属项目分类'},
 						{prop:'banner',lable:'banner',type:"img"},
+						{prop:'detail_banner',lable:'detail_banner',type:"img"},
 						{prop:'fields',lable:'领域范围'},
 						{prop:'expected_profit',lable:'预计收益'},
 						{prop:'extra_reward',lable:'额外赏金'},
@@ -703,6 +713,7 @@
 				//console.log(row);
 				this.form.classify_id = row.classify_id;
 				this.form.banner = row.banner;
+				this.form.detail_banner = row.detail_banner;
 				this.form.fields = row.fields;
 				this.checkedroles = row.fields.split(",");
 				this.form.expected_profit = row.expected_profit;
@@ -1016,6 +1027,37 @@
 				});
 				//console.log(this.form.banner = url)
 			},
+			httprequestbanner(params) {
+				const _file = params.file;
+				let app_secret = '1Q61s1iP8I376GyMTdsjOzd4hcLpZ4SG';
+				let open_id = 7;
+				let times = (Date.parse(new Date()) / 1000);
+				let arr = [
+					1003,
+					app_secret,
+					open_id,
+					times
+				];
+
+				// 通过 FormData 对象上传文件
+				var formData = new FormData();
+				formData.append("file", _file);
+				formData.append('app_id', 1003);
+				formData.append('sign', this.MD5(encodeURIComponent(arr.sort())))
+				formData.append('user', open_id)
+				formData.append('relation_type', 'activity')
+				formData.append('timestamp', times)
+			    var _this = this;
+				this.$parent.setpercentage("start");
+				this.axios.post('http://139.129.221.123/File/File/insert', formData).then(function (response) {
+					_this.uptype = "detail_banner";
+					_this.$parent.setpercentage("end",response.data.data.url);
+					//_this.form.banner = response.data.data.url
+				}).catch(function (error) {
+					console.log(error);
+				});
+				//console.log(this.form.banner = url)
+			},
 			httprequestcover(params) {
 				const _file = params.file;
 				let app_secret = '1Q61s1iP8I376GyMTdsjOzd4hcLpZ4SG';
@@ -1051,6 +1093,10 @@
 				if(this.uptype == 'banner'){
 					
 					this.form.banner = url;
+				}
+				if(this.uptype == 'detail_banner'){
+					
+					this.form.detail_banner = url;
 				}
 				
 				if(this.uptype == 'cover'){
@@ -1133,6 +1179,7 @@
 				}).then(da=>{
 					this.form.classify_id = da.classify_id;
 					this.form.banner = da.banner;
+					this.detail_banner = da.detail_banner;
 					this.form.fields = da.fields;
 					this.checkedroles = da.fields.split(',');
 					this.form.expected_profit = da.expected_profit;
@@ -1468,6 +1515,10 @@
 					
 					return "请上传banner！！";
 				}
+				if(!this.form['detail_banner']){
+					
+					return "请上传详情banner！！";
+				}
 				
 				if(!this.form['fields']){
 					
@@ -1521,6 +1572,7 @@
 							this.rows = "";
 							this.form.classify_id = da.classify_id;
 							this.form.banner = da.banner;
+							this.form.detail_banner = da.detail_banner;
 							this.form.fields = da.fields;
 							this.checkedroles = da.fields.split(',');
 							this.form.expected_profit = da.expected_profit;
