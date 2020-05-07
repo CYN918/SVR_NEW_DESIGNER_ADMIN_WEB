836e3fe849a0964223e2be572c9aee3f01012b9b
sanquan <470980592@qq.com>
2020-05-07 10:56:14 +0800

    20200507

--- src/views/review/projectdetial.vue
+++ src/views/review/projectdetial.vue
@@ -815,13 +815,13 @@
 				
 			},
 			up(){
-				fetch(this.material_info.download_file_url).then(res => res.blob()).then(blob => {
+				fetch(this.material_info.download_file_url).then(res => {
 					const a = document.createElement('a');
 					document.body.appendChild(a)
 					a.style.display = 'none'
 					// 使用获取到的blob对象创建的url
-					const url = window.URL.createObjectURL(blob);
-					a.href = url;
+					// const url = window.URL.createObjectURL(blob);
+					a.href = res;
 					// 指定下载的文件名
 					a.download = this.material_info.file_name;
 					a.click();
