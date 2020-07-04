677f69b175cc71c476bf1c00e2f6f485e5174b7e
wukuangyu <964453362@qq.com>
2020-07-04 17:04:20 +0800

    修改bug.850 后台-审核台-项目验收-价格审核时，选择分成的填写比例的输入框，后面的“%”符号离得太远了

--- src/views/review/projectdetial.vue
+++ src/views/review/projectdetial.vue
@@ -245,9 +245,15 @@
 					<li class="w ofh" v-if="want_deal_type == '2' || want_deal_type == '3'">
 						<div class="textcenter employipt">
 							<span class="fleft Dialogkey" style="width: 84px;text-align: right;">分成比例</span>
-							<el-input style="width: 400px" class="fleft sel-dialog-content" placeholder="按掌酷各渠道收益，请输入0-100数字(单位:%)" v-model="user_split_rate"
-							 clearable>
-							</el-input>%
+							<el-input
+								style="width: 400px"
+								class="fleft sel-dialog-content"
+								placeholder="按掌酷各渠道收益，请输入0-100数字"
+								v-model="user_split_rate"
+							 	clearable
+							>
+								<template slot="suffix">%</template>
+							</el-input>
 						</div>
 					</li>
 					<li class="w ofh" v-if="want_deal_type == '3'">
